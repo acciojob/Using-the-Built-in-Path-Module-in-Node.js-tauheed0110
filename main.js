4ad66ec0e177e8426db0a881e0372300f762bbc1
saif0110 <khanmohdtauheed4@gmail.com>
2025-01-01 10:21:47 +0530

    file changed

--- main.js
+++ main.js
@@ -1,5 +1,16 @@
 const path = require('path');
 
 // TODO: Use path.join to join two file path segments
+function joinPaths(path1, path2){
+    return path.join(path1, path2);
 
-// TODO: Print the resulting path to the console
+}
+
+const path1 = process.argv[2];
+const path2 = process.argv[3];
+
+if(!path1 || !path2){
+    console.log('Provide two path to add them, command line argv');
+    process.exit(1);
+}
+console.log(joinPaths(path1, path2));
